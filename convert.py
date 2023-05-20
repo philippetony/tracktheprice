365de8be2105f4bd860f53a7728b387accb343a0
Philippe TONY <philippe.tony@zenika.com>
2023-05-20 11:19:49 +0200

    refacto: changing folder structure

--- convert.py
+++ convert.py
@@ -3,33 +3,40 @@ import os
 from pathlib import Path
 from pytesseract import image_to_pdf_or_hocr, image_to_string
 
-target_path = Path("data")
-def convert_systeme_u(target):
-    for item in os.listdir(target):
+input_path = Path("input")
+temp_path = Path("temp")
+
+def convert_systeme_u(src, temp):
+    if not temp.exists():
+        temp.mkdir(parents=True)
+    for item in os.listdir(src):
         if item.endswith('.pdf'):
-            with open(target/item,"rb") as pdf_file:
+            with open(src/item,"rb") as pdf_file:
                 pdf = pdftotext.PDF(pdf_file)
                 if (len(pdf) == 1):
-                    with open(target/item.replace('pdf','u.txt'), "w+") as txt:
+                    with open(temp/item.replace('pdf','u.txt'), "w+") as txt:
                         txt.write(pdf[0])
 
-def convert_lidl(target):
-    for item in os.listdir(target):
-        if item.endswith('.jpg'):
-            string_content = image_to_string(str(target/item))
-            print(string_content)
-            content = image_to_pdf_or_hocr(str(target/item))
+def convert_lidl(src,temp,extension='jpg'):
+    if not temp.exists():
+        temp.mkdir(parents=True)
+    for item in os.listdir(src):
+        if item.endswith(f'.{extension}'):
+            string_content = image_to_string(str(src/item))
+            # print(string_content)
+            # content = image_to_pdf_or_hocr(str(target/item))
             # if (len(pdf) == 1):
-            #     with open(target/item.replace('pdf','lidl.txt'), "w+") as txt:
-            #         txt.write(pdf[0])
+            with open(temp/(item.replace(extension,'lidl.txt')), "w+") as txt:
+                txt.write(string_content)
             # print(content)
-            with open(target/item.replace('.jpg','lidl.pdf'), 'wb+') as f:
-                f.write(content)
-            with open(target/item.replace('.jpg','lidl.pdf'), 'rb') as f:
-                pdf = pdftotext.PDF(f)
-                print(pdf[0])
-            break
+            # with open(target/item.replace('.jpg','lidl.pdf'), 'wb+') as f:
+            #     f.write(content)CACAT
+            # with open(target/item.replace('.jpg','lidl.pdf'), 'rb') as f:
+            #     pdf = pdftotext.PDF(f)
+            #     print(pdf[0])
+            # break
 
 if __name__ == "__main__":
-    # convert_systeme_u(target_path)
-    convert_lidl(target_path)
+    convert_systeme_u(input_path/"u", temp_path/"u")
+    convert_lidl(input_path/"lidl", temp_path/"lidl")
+    convert_lidl(input_path/"lidl", temp_path/"lidl", 'png')
