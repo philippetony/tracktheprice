f8575bee7dea14ef2237436c6b63c308ca2063ff
Philippe TONY <philippe.tony@zenika.com>
2023-05-20 12:15:47 +0200

    feat: lidl extraction

--- extract.py
+++ extract.py
@@ -4,9 +4,12 @@ from pathlib import Path
 from model import Ticket, ArticlePrice
 import datetime
 
-target_path=Path('data')
+temp_path=Path('temp')
 single_line_systeme_u_pattern = ' *([A-Z0-9 ./()&%-]+) +([0-9,]+) € +[0-9]+ *$'
 multi_line_systeme_u_pattern = ' *(\d+) +x +([0-9,]+) € +([0-9,]+) € +[0-9]+ *$'
+
+single_line_lidl_pattern = ' *([\w\d ./()&%-]+) +([0-9,]+) [xX]+ ([0-9]+) ([0-9,]+) [AB] *$'
+
 
 def extract_systeme_u(target):
     res = []
@@ -35,18 +38,18 @@ def extract_lidl(target):
             with open(target/item, 'r') as ticket:
                 lines = ticket.readlines()
                 for line_number, line in enumerate(lines):
-                    if match := re.match(single_line_systeme_u_pattern, line):
+                    if match := re.match(single_line_lidl_pattern, line):
                         fusion_groups = [x.strip() for x in match.groups()]
                         parsed_ticket.prices.append(ArticlePrice(fusion_groups[0], float(fusion_groups[1].replace(',','.'))))
-                    elif match := re.match(multi_line_systeme_u_pattern, line):
-                        fusion_groups=[lines[line_number-1].strip(), match.groups()[1]]
-                        parsed_ticket.prices.append(ArticlePrice(fusion_groups[0], float(fusion_groups[1].replace(',','.'))))
-                    elif (date_match := re.search("(\d{2}\/\d{2}\/\d{2})", line)) and (re.search("Date", lines[line_number-1])):
-                        parsed_ticket.date = datetime.datetime.strptime(date_match.group(0), "%d/%m/%y").date()
+                    if (match:= re.match(" *Rem ([\w\d ./()&%-]+) *(-[0-9,]+)", line)) and len(parsed_ticket.prices) > 0:
+                        parsed_ticket.prices[-1].price += float(match.group(2).replace(',','.'))
+                    elif (date_match := re.search("(\d{2}.\d{2}.\d{2}) \d{2}:\d{2}:\d{2}", line)):
+                        parsed_ticket.date = datetime.datetime.strptime(date_match.group(1), "%d.%m.%y").date()
             res.append(parsed_ticket)
     return res
 
 if __name__ == '__main__':
-    tickets = extract_systeme_u(target_path)
-    tickets += extract_lidl(target_path)
+    tickets = []
+    # tickets += extract_systeme_u(temp_path/"u")
+    tickets += extract_lidl(temp_path/"lidl")
     print(tickets)
--- model.py
+++ model.py
@@ -11,3 +11,9 @@ class ArticlePrice:
 class Ticket:
     date: date = None
     prices: List[ArticlePrice] = field(default_factory = lambda: [])
+
+    def __repr__(self):
+        s = f"\nDate: {self.date}\n"
+        for item in self.prices:
+            s+=f"{item.article_name:<30}{item.price:>6}\n"
+        return s
